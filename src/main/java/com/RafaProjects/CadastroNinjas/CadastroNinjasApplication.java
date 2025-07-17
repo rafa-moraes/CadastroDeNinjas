da899860d136ad602c8fbd022d43e76a992bbe93
Rafael Moraes Barros <rafael_m_barros@hotmail.com>
2025-07-17 18:56:02 +0100

    First commit

---
+++ src/main/java/com/RafaProjects/CadastroNinjas/CadastroNinjasApplication.java
@@ -0,0 +1,14 @@
+package com.RafaProjects.CadastroNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroNinjasApplication.class, args);
+	}
+
+
+}
---
+++ src/main/java/com/RafaProjects/CadastroNinjas/Controller.java
@@ -0,0 +1,17 @@
+package com.RafaProjects.CadastroNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping ("/Hello")
+    public String hello (){
+        return "Hello World";
+    }
+
+}
